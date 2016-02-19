4bea9a3e53325e8747c2da6f5e6a22ec851d4fe2
Ayush <ayush@localhost.localdomain>
2016-02-19 19:33:00 +0530

    First Commit

--- abc.py
+++
@@ -1,8 +0,0 @@
-import dbus
-bus = dbus.SessionBus()
-notifications = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
-interface = dbus.Interface(notifications, 'org.freedesktop.Notifications')
-id = 4856
-timeout = 2500
-interface.Notify('name',id,'','Hi Raunaq','body','','',timeout)
-
